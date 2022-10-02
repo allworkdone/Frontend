39cce4d78e852485a32409863679cf06d5dd14aa
allworkdone <raj635321@gmail.com>
2022-10-02 14:11:00 +0530

    C++ :Added Code for InsertionSort The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
    First line of each test case or query contains an integer 'N' representing the size of the array/list.
    
    Second line contains 'N' single space separated integers representing the elements in the array/list.

---
+++ InsertionSort.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int *a, int size)
+{
+    for(int i = 1; i < size; i++)
+    {
+	int temp = a[i];
+    	int j= i-1;
+    	for (; j>=0; j--)
+    	{
+        	if (a[j]>temp){
+            	a[j+1 ]= a[j];
+        	}
+        	else{
+            		break;
+        	}
+    	}
+    a[j+1] = temp;
+    }
+}
+
+int main()
+{
+	//For number of Test cases
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int size;
+		cin >> size;
+		int *input = new int[size];
+		//taking input to the array
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+		//Function calling
+		insertionSort(input, size);
+
+		//Printing the sorted array
+		for (int i = 0; i < size; i++)
+		{
+			cout << input[i] << " ";
+		}
+
+		delete[] input;
+		cout << endl;
+	}
+
+	return 0;
+}
